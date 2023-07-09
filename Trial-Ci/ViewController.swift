da685a2afe4bd31a3e3edaffc505fd638cbe6709
Icung <saiful-islam@outlook.co.id>
2023-07-09 13:57:22 +0700

    initial commit

---
+++ Trial-Ci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trial-Ci
+//
+//  Created by Icung on 09/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
